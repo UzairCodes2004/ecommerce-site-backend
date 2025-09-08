e862b8499d2f84b9456b6341513d517a420c7d64
UzairCodes2004 <raouzair70@gmail.com>
2025-09-08 12:16:18 +0500

    Updated CORS for data fetching from frontend

--- app.js
+++ app.js
@@ -30,10 +30,11 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
